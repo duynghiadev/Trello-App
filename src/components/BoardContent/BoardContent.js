71bab5e42d729bc70b64947469191bd19712dc79
duynghia-123 <duynghia22302@gmail.com>
2022-11-15 14:42:02 +0700

    initial sample data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,16 +1,39 @@
+import { initialData } from "components/actions/initialData";
 import Column from "components/Column/Column";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BoardContent.scss";
+import { isEmpty } from "lodash";
+import { mapOrder } from "utilities/sorts";
 
 function BoardContent() {
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find(
+      (board) => board.id === "board-1"
+    );
+    if (boardFromDB) {
+      setBoard(boardFromDB);
+
+      // sort column
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
+    }
+  }, []);
+
+  if (isEmpty(board)) {
+    return (
+      <div className="not-found" style={{ padding: "10px", color: "white" }}>
+        Board not found
+      </div>
+    );
+  }
+
   return (
     <div className="board-content">
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
+      {columns.map((column, index) => (
+        <Column key={index} column={column} />
+      ))}
     </div>
   );
 }
