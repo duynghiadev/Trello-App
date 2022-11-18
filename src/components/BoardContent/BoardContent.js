16fc9563e6d33597555b84ef0fa3c5fe0f414e1f
duynghia-123 <duynghia22302@gmail.com>
2022-11-19 06:46:57 +0700

    Update & remove columns

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -109,6 +109,30 @@ function BoardContent() {
     toggleOpenNewColumnForm();
   };
 
+  const onUpdateColumn = (newColumnToUpdate) => {
+    const columnIdToUpdate = newColumnToUpdate.id;
+
+    let newColumns = [...columns];
+    const columnIndexToUpdate = newColumns.findIndex(
+      (i) => i.id === columnIdToUpdate
+    );
+
+    if (newColumnToUpdate._destroy) {
+      // remove column
+      newColumns.splice(columnIndexToUpdate, 1);
+    } else {
+      // update column
+      newColumns.splice(columnIndexToUpdate, 1, newColumnToUpdate);
+    }
+
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((c) => c.id);
+    newBoard.columns = newColumns;
+
+    setColumns(newColumns);
+    setBoard(newBoard);
+  };
+
   return (
     <div className="board-content">
       <Container
@@ -124,7 +148,11 @@ function BoardContent() {
       >
         {columns.map((column, index) => (
           <Draggable key={index}>
-            <Column column={column} onCardDrop={onCardDrop} />
+            <Column
+              column={column}
+              onCardDrop={onCardDrop}
+              onUpdateColumn={onUpdateColumn}
+            />
           </Draggable>
         ))}
       </Container>
