43b573f5c075e866d4d6fb00b9df30511b308d34
duynghia-123 <duynghia22302@gmail.com>
2022-11-22 05:37:51 +0700

    Combine api get full board, handle cors

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -14,7 +14,7 @@ import { cloneDeep } from "lodash";
 
 function Column(props) {
   const { column, onCardDrop, onUpdateColumn } = props;
-  const cards = mapOrder(column.cards, column.cardOrder, "id");
+  const cards = mapOrder(column.cards, column.cardOrder, "_id");
 
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const toggleShowConfirmModal = () => setShowConfirmModal(!showConfirmModal);
@@ -72,14 +72,14 @@ function Column(props) {
     const newCardToAdd = {
       id: Math.random().toString(36).substr(2, 5),
       boardId: column.boardId,
-      columnId: column.id,
+      columnId: column._id,
       title: newCardTitle.trim(),
       cover: null,
     };
 
     let newColumn = cloneDeep(column);
     newColumn.cards.push(newCardToAdd);
-    newColumn.cardOrder.push(newCardToAdd.id);
+    newColumn.cardOrder.push(newCardToAdd._id);
 
     onUpdateColumn(newColumn);
     setNewCardTitle("");
@@ -132,7 +132,7 @@ function Column(props) {
         <Container
           orientation="vertical"
           groupName="duynghiadev-columns"
-          onDrop={(dropResult) => onCardDrop(column.id, dropResult)}
+          onDrop={(dropResult) => onCardDrop(column._id, dropResult)}
           getChildPayload={(index) => cards[index]}
           dragClass="card-ghost"
           dropClass="card-ghost-drop"
