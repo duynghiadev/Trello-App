06ea7ca2c493b00226cbce07e5551ffe369ae1c7
duynghiadev <duynghia22302@gmail.com>
2024-02-09 15:58:49 +0700

    61. Axios: Gọi API từ phía Front-end sao cho Clean Code | NodeJS + MongoDB | TrungQuanDev

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,8 +3,21 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // Tạm thời fix  cứng boardId, flow chuẩn chỉnh về sau khi học nâng cao trực tiếp vơi minh là chúng ta sẽ sử dụng react-router-dom để lấy chuẩn boardId từ URL về
+    const boardId = '653287d058a7da7b82902826'
+    // Call API
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container
       disableGutters
@@ -14,8 +27,8 @@ function Board() {
       }}
     >
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
