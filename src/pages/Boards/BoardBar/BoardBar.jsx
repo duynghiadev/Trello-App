36c763bc8559c60742e0eb572e39777dbddd79fa
duynghiadev <duynghia22302@gmail.com>
2024-02-09 19:25:29 +0700

    63. Aggregate: Query tổng hợp, Join dữ liệu giữa các Collections | NodeJS + MongoDB | TrungQuanDev

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -38,26 +38,58 @@ function BoardBar({ board }) {
         gap: 2,
         paddingX: 2,
         overflowX: 'auto',
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2')
+        bgcolor: (theme) =>
+          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'
       }}
     >
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <Chip sx={MENU_STYLES} icon={<DashboardIcon />} label={board?.title} clickable />
+        <Tooltip title={board?.description}>
+          <Chip
+            sx={MENU_STYLES}
+            icon={<DashboardIcon />}
+            label={board?.title}
+            clickable
+          />
+        </Tooltip>
 
-        <Chip sx={MENU_STYLES} icon={<VpnLockIcon />} label={capitalizeFirstLetter(board?.type)} clickable />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label={capitalizeFirstLetter(board?.type)}
+          clickable
+        />
 
-        <Chip sx={MENU_STYLES} icon={<AddToDriveIcon />} label='Add To Google Drive' clickable />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label='Add To Google Drive'
+          clickable
+        />
 
-        <Chip sx={MENU_STYLES} icon={<BoltIcon />} label='Automation' clickable />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label='Automation'
+          clickable
+        />
 
-        <Chip sx={MENU_STYLES} icon={<FilterListIcon />} label='Filters' clickable />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label='Filters'
+          clickable
+        />
       </Box>
 
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <Button
           variant='outlined'
           startIcon={<PersonAddIcon />}
-          sx={{ color: 'white', borderColor: 'white', '&:hover': { borderColor: 'white' } }}
+          sx={{
+            color: 'white',
+            borderColor: 'white',
+            '&:hover': { borderColor: 'white' }
+          }}
         >
           Invite
         </Button>
