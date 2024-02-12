85fe8fd49a1f3dc03285c530530a9bc20750514c
duynghiadev <duynghia22302@gmail.com>
2024-02-12 11:12:40 +0700

    75. Deploy miễn phí NodeJS Back-end APIs lên Render | ReactJS, NodeJS, MongoDB | TrungQuanDev

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
+// Port khi chạy Local trên máy tính
 export const API_ROOT = 'http://localhost:8017'
+
+/**
+ * - Port khi mình đẩy code server lên cloud (dùng Render.com) -> đang gặp lỗi thêm thẻ tín dụng thì mới được dùng Render.com -> đang tìm phương án thay thế
+ * - Nếu deploy server lên cloud được rồi thì trên web đó nó sẽ generate cho mình 1 cái link, sau đó mình sẽ lấy cái link đó thay thế vào API_ROOT là xong
+ */
+
+// export const API_ROOT = ''
